7c6ab52321712128a7d3d6809588a6b4b1cf6518
Meshicage <89930518+Meshicage228@users.noreply.github.com>
2023-04-08 16:10:41 +0300

    Add files via upload

---
+++ HashingLinVar10/Header.h
@@ -0,0 +1,10 @@
+#pragma once
+struct box;
+unsigned int hashing(int);
+void init_hash_table();
+void print_table();
+void hash_table_instert(box*);
+int search_for_box(int);
+void hesh_delete_point(int);
+void show_priority(int);
+
---
+++ HashingLinVar10/Work.cpp
@@ -0,0 +1,34 @@
+﻿#include "Header.h"
+#include <iostream>
+using namespace std;
+
+struct box {
+    int num;
+    int index;
+    bool priority;
+};
+
+int main()
+{
+    init_hash_table();
+    srand(time(NULL));
+    cout << "Number of nums <20 : ";
+    int n; cin >> n;
+    box* arr = new box[n];
+    print_table();
+    cout << "Array : \n";
+    for (int i = 0; i < n; i++) {
+        arr[i].num = rand() % 901 + 100;
+        cout << i << " ---- " << arr[i].num << endl;
+        hash_table_instert(&arr[i]);
+    }
+    cout << "HashTable :\n";
+    print_table();
+    cout << "From top to niz : \n"; 
+    show_priority(n);
+    cout << endl;
+    int y;
+    cin >> y;
+    search_for_box(y);
+    return 0;
+}
