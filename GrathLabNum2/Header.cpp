85d9ef1387f4d66aa2938624a2c93cf7b3d3fda5
Meshicage <89930518+Meshicage228@users.noreply.github.com>
2023-04-19 21:28:46 +0300

    TODO reade

--- GrathLabNum2/Header.cpp
+++ GrathLabNum2/Header.cpp
@@ -4,126 +4,142 @@ using namespace std;
 
 #define N 9
 
-struct Node {
+bool** adjacency_array = new bool* [N];
+
+struct Element {
     int source;
-    int dest;
-    Node* next;
+    int destination;
+    Element* next;
     int priority;
 };
 struct LinkedGrath {
-    Node* head[N];
+    Element* head[N];
 };
 struct Edge {
-    int source_num, dest_num, prior;
+    int source_num, dest_num, edge;
 };
-LinkedGrath* createGrath(Edge edges[], int n) {
+LinkedGrath* CreateGrath(Edge edges[], int n) {
     LinkedGrath* ptr = new LinkedGrath();
     for (int i = 0; i < N; i++) {
         ptr->head[i] = NULL;
     }
     for (int i = 0; i < n; i++) {
-        int src = edges[i].source_num;
-        int dest = edges[i].dest_num;
-        int node = edges[i].prior;
-        Node* newNode = new Node();
-        newNode->dest = dest;
-        newNode->source = src;
+        Element* newNode = new Element();
+
+        int main_vertex = edges[i].source_num,
+            neighboring_vertex = edges[i].dest_num,
+            node = edges[i].edge;
+
+        newNode->destination = neighboring_vertex;
+        newNode->source = main_vertex;
         newNode->priority = node;
-        newNode->next = ptr->head[src];
-        ptr->head[src] = newNode;
+        newNode->next = ptr->head[main_vertex];
+        ptr->head[main_vertex] = newNode;
     }
     return ptr;
 }
-void printGrath(LinkedGrath* LinkedGrath) {
+void PrintGrath(LinkedGrath* LinkedGrath) {
     for (int i = 0; i < N; i++) {
-        Node* ptr = LinkedGrath->head[i];
+        Element* ptr = LinkedGrath->head[i];
         while (ptr != NULL) {
-            cout << i << " -> " << ptr->dest << "(" << ptr->priority << ")" << "  ";
+            cout << i << " -> " << ptr->destination << "(" << ptr->priority << ")" << "  ";
             ptr = ptr->next;
         }
         cout << endl;
     }
     cout << endl;
 }
-
-void ShowMatrix(LinkedGrath* grath) {
-    bool** arr = new bool* [N];
-    for (int i = 0; i < N; i++) {
-        arr[i] = new bool[N];
-    }
-    for (int i = 0; i < N; i++) {
-        for (int j = 0; j < N; j++) {
-            arr[i][j] = false;
-        }
-    }
-    cout << endl;
-    for (int i = 1; i < N; i++)
-    {
-        if (grath->head[i] == NULL) continue;
-        Node* ptr = grath->head[i];
-        while (ptr)
-        {
-            arr[i][ptr->dest] = true;
-            ptr = ptr->next;
-        }
-    }
-    for (int i = 0; i < N; i++) {
-        for (int j = 0; j < N; j++) {
-            cout << arr[i][j] << " ";
-        }
-        cout << endl;
-    }
-    cout << endl;
-}
-void deleteNodes(LinkedGrath* grath, int key) {
+void CreateEmptyMatrix() {
     for (int i = 1; i < N; i++) {
-        if (grath->head[i] == NULL) continue; 
-        Node* ptr = grath->head[i];
-        Node* nextp = ptr->next;
+        adjacency_array[i] = new bool[N];
+        for (int j = 1; j < N; j++) {
+            adjacency_array[i][j] = false;
+        }
+    }
+}
+void FillMatrixWithData(LinkedGrath* grath){
+    for (int i = 1; i < N; i++) {
+        if (grath->head[i] == NULL) continue;
+        Element* ptr = grath->head[i];
+        while (ptr) {
+            adjacency_array[i][ptr->destination] = true;
+            ptr = ptr->next;
+        }
+    }
+}
+void PrintAdjacencyMatrix() {
+    for (int i = 1; i < N; i++) {
+        for (int j = 1; j < N; j++) {
+            cout << adjacency_array[i][j] << " ";
+        }
+        cout << endl;
+    }
+    cout << endl;
+}
+void CreateAndShowAdjacencyMatrix(LinkedGrath* grath) {
+    CreateEmptyMatrix();
+    FillMatrixWithData(grath);
+    PrintAdjacencyMatrix();
+}
+// TODO create new delete algorithm for Vertex and Node functions
+void DeleteNodes(LinkedGrath* grath, int key) {
+    for (int i = 1; i < N; i++) {
+        cout << "pip " << endl;
+        if (grath->head[i] == NULL) continue;
+        Element* ptr = grath->head[i];
+        Element* nextp = ptr;
         bool flag = true;
-        while (flag) {
+        while (true) {
             nextp = ptr->next;
             if (nextp->priority == key) {
-                while (nextp) {
-                    if (nextp->priority != key) break;
-                    nextp = nextp->next;
+                do {
+                    cout << "pio" << endl;
+                    nextp = nextp->next;             
+                } while (nextp->priority == key && nextp->next); 
+                if (nextp->next == NULL) {
+                    if (ptr->next->priority == key && nextp->priority == key) {
+                        ptr->next = NULL;
+                        break;
+                    }
                 }
-                ptr->next = nextp;
-                ptr = ptr->next;
+                if(nextp->next == NULL && nextp->priority != key) {
+                    ptr->next = nextp;
+                    cout << "pio2" << endl;
+                    ptr = ptr->next;
+                }
             }
             else {
                 ptr = ptr->next;
-                nextp = ptr->next;
             }
-            if (ptr == NULL || ptr->next == NULL) flag = false;
+            if (ptr == NULL || nextp->next == NULL) break;
         }
     }
 }
-void deleteVerx(LinkedGrath* grath, int key) {
+void DeleteVertex(LinkedGrath* grath, int key) {
     for (int i = 1; i < N; i++) {
+        cout << "pip " << endl;
         if (grath->head[i] == NULL) continue;
-        Node* ptr = grath->head[i];
-        Node* nextp = ptr->next;
+        Element* ptr = grath->head[i];
+        Element* nextp = ptr->next;
         if (ptr->source == key) {
             grath->head[i] = NULL;
         }
         else {
-            bool flag = true;
-            while (flag) {
+            while (true) {
                 nextp = ptr->next;
-                if (nextp->source == key || nextp->dest == key) {
-                    while (nextp) {
-                        if (nextp->source != key && nextp->dest != key) break;
+                if (nextp->destination == key) {
+                    do {
+                        cout << "pio" << endl;
                         nextp = nextp->next;
-                    }
+                    } while (nextp->destination == key && nextp);
                     ptr->next = nextp;
+                    cout << "pio2" << endl;
                     ptr = ptr->next;
                 }
                 else {
                     ptr = ptr->next;
-                    nextp = ptr->next;
                 }
-                if (ptr == NULL || ptr->next == NULL) flag = false;
+                if (ptr == NULL || nextp->next == NULL) break;
             }
         }
     }
