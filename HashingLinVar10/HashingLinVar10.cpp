abb3a54ce0439174acc22bbb8e5ee99e8acd1a4e
Meshicage <89930518+Meshicage228@users.noreply.github.com>
2023-04-20 20:16:04 +0300

    Menu

--- HashingLinVar10/HashingLinVar10.cpp
+++ HashingLinVar10/HashingLinVar10.cpp
@@ -1,56 +1,64 @@
-﻿#include "Header.h"
+#include "Header.h"
 #include <iostream>
 using namespace std;
 
-struct box {
-    int num;
+struct Element {
+    int number;
     int index;
     bool priority;
 };
-
-int main()
-{
-    init_hash_table();
-    srand(time(NULL));
-    setlocale(0, "rus");
+int SetSizeOfArray() {
     int size = 0;
     do {
-        cout << "Введите количество рандомных чисел < 20 :";
+        cout << "Enter the number of random numbers, which < 20 :";
         cin >> size;
     } while (size < 0 || size >= 20);
-    box* arr = new box[size];
-    print_table();
+    return size;
+}
+void CreateHashTable(int size) {
+    InitializeHashTable();
+
+    Element* arr = new Element[size];
+
+    PrintHashTable();
+
     cout << "Array : \n";
     for (int i = 0; i < size; i++) {
-        arr[i].num = rand() % 1201 + 100;
-        cout << i << " ---- " << arr[i].num << endl;
-        hash_table_instert(&arr[i]);
+        arr[i].number = rand() % 1201 + 100;
+        cout << i << " ---- " << arr[i].number << endl;
+        PushElementToHashTable(&arr[i]);
     }
+}
+int main() {
+    
+    srand(time(NULL));
+    setlocale(0, "rus");
+
+    int size = SetSizeOfArray();
+    CreateHashTable(size);
+
     do {
-        cout << "1.Хеш-таблица\n2.Печать элементов в порядке убывания ключа\n3.Найти элемент хеш-таблицы\n4.Удалить введеное число\n5.Выход\nОпция:";
+        cout << "1.Print HashTable\n2.Write numbers in descending order\n3.Find number in HashTable\n4.Delete entered number\n5.Exit\nOption:";
         int option;
         cin >> option;
         cout << endl;
-        switch (option)
-        {
-        case 1: print_table();
-             break;
-        case 2: { cout << "Элементы в порядке убывания ключа : \n";  show_priority(size); cout << endl;
+        switch (option) {
+        case 1: PrintHashTable();
+            break;
+        case 2: { cout << "HashIndexes    Keys : \n";  WriteInDescendingOrder(size); cout << endl;
             break; }
-        case 3: { cout << "Введите число : ";
-            int y;
-            cin >> y;
-            int index = search_for_box(y);
-            if (index == -1) cout << "Такого числа не существует...\n";
-            else cout << "Число " << y << " обнаружено в хеш-индекске " << index << endl;
+        case 3: { cout << "Write number to search : ";
+            int y; cin >> y;
+            int index = SearchNumber(y);
+            if (index == NULL) cout << "There is no such number...\n";
+            else cout << "Number " << y << " detected at index :  " << index << endl;
             break;
         }
-        case 4: cout << "Введите число : "; int x; cin >> x;
-              hesh_delete_point(x);
-              size--;
-             break;
-        case 5: cout << "Выход..."; return 0;
-        default: cout << "Неверная опция...\n";
+        case 4: cout << "Write number which you need to delete : "; int x; cin >> x;
+            DeleteNumber(x);
+            break;
+        case 5: cout << "Exit..."; return 0;
+        default: cout << "Wrong operation...\n";
             break;
         }
     } while (true);
