997d0c0eee8a5c777676e6d7de03b294825ae5b1
Meshicage <89930518+Meshicage228@users.noreply.github.com>
2023-04-20 18:44:13 +0300

    Methods to Implement in cpp

--- GrathLabNum2/Header.h
+++ GrathLabNum2/Header.h
@@ -5,8 +5,15 @@ struct LinkedGrath;
 struct Edge;
 LinkedGrath* CreateGrath(Edge[], int);
 void PrintGrath(LinkedGrath*);
-void DeleteNodes(LinkedGrath*, int);
-void DeleteVertex(LinkedGrath*, int);
+
+void NodeToDelete(LinkedGrath*, int);
+void DeleteNode(Element* ptrs, int);
+
+void VertexToDelete(LinkedGrath*, int);
+void DeleteVertex(Element*, int);
+
+Element* DeleteNodeOrVertexAroundHead(Element*, int); // Delete first keys if they exist and remove the beggining of list
+
 
 void CreateAndShowAdjacencyMatrix(LinkedGrath*);
 // Components of AdjacencyMatrix
